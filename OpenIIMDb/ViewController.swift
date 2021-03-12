9e753d4fec7734d149e6d454e09c8b5b7452d0c9
a-elnajjar <code@mail.com>
2021-03-11 20:31:25 -0500

    Initial Commit

---
+++ OpenIIMDb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenIIMDb
+//
+//  Created by Abdalla Elnajjar on 2021-03-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
